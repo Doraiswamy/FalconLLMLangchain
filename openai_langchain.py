932f4d0a76481394464b7ce92bbf3180432a5270
Nachiketh DORAISWAMY <nachiketh_d@nec.com.sg>
2024-01-25 10:21:14 +0800

    latest master

--- openai_langchain.py
+++ openai_langchain.py
@@ -33,10 +33,6 @@ def chainLoad():
     vectorStore = FAISS.from_texts(chunks, embeddings)
     template = """
                     I'll provide you with some context and history related to a question. Please carefully consider the information and answer the question thoughtfully. If the answer isn't found in the context, use your extensive knowledge and understanding of the world to provide a comprehensive response.
-                    
-                    Here are some RULES YOU NEED TO FOLLOW in order of importance:
-                    1. If the user query explicitly mentions any request to create an incident like for eg. 'create incident', return the word 'create_incident_form' exactly without checking the history or context.
-                    2. If the user query is about information related to incident creation like for eg. 'how to create an incident', provide a comprehensive response.
 
                     Context:
                     {context}
